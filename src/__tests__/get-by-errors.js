6544843631902b0431bd79d5013814877c00c6e4
Xavier Garcia Buils <xgbuils@gmail.com>
2020-02-01 17:06:40 +0100

    fix(ByDisplayValue): improve warning message

--- src/__tests__/get-by-errors.js
+++ src/__tests__/get-by-errors.js
@@ -1,86 +1,103 @@
-import cases from 'jest-in-case'
 import {render} from './helpers/test-utils'
 
-cases(
-  'getBy* queries throw an error when there are multiple elements returned',
-  ({name, query, html}) => {
-    const utils = render(html)
-    expect(() => utils[name](query)).toThrow(/multiple elements/i)
-  },
-  {
-    getByLabelText: {
-      query: /his/,
-      html: `<div aria-label="his"></div><div aria-label="history"></div>`,
-    },
-    getByPlaceholderText: {
-      query: /his/,
-      html: `<input placeholder="his" /><input placeholder="history" />`,
-    },
-    getByText: {
-      query: /his/,
-      html: `<div>his</div><div>history</div>`,
-    },
-    getByAltText: {
-      query: /his/,
-      html: `<img alt="his" src="his.png" /><img alt="history" src="history.png" />`,
-    },
-    getByTitle: {
-      query: /his/,
-      html: `<div title="his"></div><div title="history"></div>`,
-    },
-    getByDisplayValue: {
-      query: /his/,
-      html: `<input value="his" /><select><option value="history">history</option></select>`,
-    },
-    getByRole: {
-      query: /his/,
-      html: `<div role="his"></div><div role="history"></div>`,
-    },
-    getByTestId: {
-      query: /his/,
-      html: `<div data-testid="his"></div><div data-testid="history"></div>`,
-    },
-  },
-)
+describe('getBy* queries throw an error when there are multiple elements returned', () => {
+  test('getByLabelText', () => {
+    const {getByLabelText} = render(
+      `<div aria-label="his"></div><div aria-label="history"></div>`,
+    )
+    expect(() => getByLabelText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('getByPlaceholderText', () => {
+    const {getByPlaceholderText} = render(
+      `<input placeholder="his" /><input placeholder="history" />`,
+    )
+    expect(() => getByPlaceholderText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('getByText', () => {
+    const {getByText} = render(`<div>his</div><div>history</div>`)
+    expect(() => getByText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('getByAltText', () => {
+    const {getByAltText} = render(
+      `<img alt="his" src="his.png" /><img alt="history" src="history.png" />`,
+    )
+    expect(() => getByAltText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('getByTitle', () => {
+    const {getByTitle} = render(
+      `<div title="his"></div><div title="history"></div>`,
+    )
+    expect(() => getByTitle(/his/)).toThrow(/multiple elements/i)
+  })
+  test('getByDisplayValue', () => {
+    const {getByDisplayValue} = render(
+      `<input value="his" /><select><option value="history">history</option></select>`,
+    )
+    expect(() => getByDisplayValue(/his/)).toThrow(
+      /multiple elements with the display value:/i,
+    )
+  })
+  test('getByRole', () => {
+    const {getByRole} = render(
+      `<div role="his"></div><div role="history"></div>`,
+    )
+    expect(() => getByRole(/his/)).toThrow(/multiple elements/i)
+  })
+  test('getByTestId', () => {
+    const {getByTestId} = render(
+      `<div data-testid="his"></div><div data-testid="history"></div>`,
+    )
+    expect(() => getByTestId(/his/)).toThrow(/multiple elements/i)
+  })
+})
 
-cases(
-  'queryBy* queries throw an error when there are multiple elements returned',
-  ({name, query, html}) => {
-    const utils = render(html)
-    expect(() => utils[name](query)).toThrow(/multiple elements/i)
-  },
-  {
-    queryByLabelText: {
-      query: /his/,
-      html: `<div aria-label="his"></div><div aria-label="history"></div>`,
-    },
-    queryByPlaceholderText: {
-      query: /his/,
-      html: `<input placeholder="his" /><input placeholder="history" />`,
-    },
-    queryByText: {
-      query: /his/,
-      html: `<div>his</div><div>history</div>`,
-    },
-    queryByAltText: {
-      query: /his/,
-      html: `<img alt="his" src="his.png" /><img alt="history" src="history.png" />`,
-    },
-    queryByTitle: {
-      query: /his/,
-      html: `<div title="his"></div><div title="history"></div>`,
-    },
-    queryByDisplayValue: {
-      query: /his/,
-      html: `<input value="his" /><select><option value="history">history</option></select>`,
-    },
-    queryByRole: {
-      query: /his/,
-      html: `<div role="his"></div><div role="history"></div>`,
-    },
-    queryByTestId: {
-      query: /his/,
-      html: `<div data-testid="his"></div><div data-testid="history"></div>`,
-    },
-  },
-)
+describe('queryBy* queries throw an error when there are multiple elements returned', () => {
+  test('queryByLabelText', () => {
+    const {queryByLabelText} = render(
+      `<div aria-label="his"></div><div aria-label="history"></div>`,
+    )
+    expect(() => queryByLabelText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('queryByPlaceholderText', () => {
+    const {queryByPlaceholderText} = render(
+      `<input placeholder="his" /><input placeholder="history" />`,
+    )
+    expect(() => queryByPlaceholderText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('queryByText', () => {
+    const {queryByText} = render(`<div>his</div><div>history</div>`)
+    expect(() => queryByText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('queryByAltText', () => {
+    const {queryByAltText} = render(
+      `<img alt="his" src="his.png" /><img alt="history" src="history.png" />`,
+    )
+    expect(() => queryByAltText(/his/)).toThrow(/multiple elements/i)
+  })
+  test('queryByTitle', () => {
+    const {queryByTitle} = render(
+      `<div title="his"></div><div title="history"></div>`,
+    )
+    expect(() => queryByTitle(/his/)).toThrow(/multiple elements/i)
+  })
+  test('queryByDisplayValue', () => {
+    const {queryByDisplayValue} = render(
+      `<input value="his" /><select><option value="history">history</option></select>`,
+    )
+    expect(() => queryByDisplayValue(/his/)).toThrow(
+      /multiple elements with the display value:/i,
+    )
+  })
+  test('queryByRole', () => {
+    const {queryByRole} = render(
+      `<div role="his"></div><div role="history"></div>`,
+    )
+    expect(() => queryByRole(/his/)).toThrow(/multiple elements/i)
+  })
+  test('queryByTestId', () => {
+    const {queryByTestId} = render(
+      `<div data-testid="his"></div><div data-testid="history"></div>`,
+    )
+    expect(() => queryByTestId(/his/)).toThrow(/multiple elements/i)
+  })
+})
